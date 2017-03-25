644dee0fd5d9604124fa1b8206510e447330639b
Jon Nehring <jwn@stonetip.com>
2017-03-25 15:32:21 -0600

    Created outlets for all UI elements

--- locTestA/locTestA/ViewController.swift
+++ locTestA/locTestA/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var labelLatVal: UILabel!
+    @IBOutlet weak var labelLonVal: UILabel!
+    @IBOutlet weak var labelAltVal: UILabel!
+    @IBOutlet weak var labelCourseVal: UILabel!
+    @IBOutlet weak var labelHeadingVal: UILabel!
+    
+    @IBOutlet weak var btnStart: UIButton!
+    @IBOutlet weak var btnStop: UIButton!
+    
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
