ad4371bd7c953923ad5493da20ae6fc304c22bf9
Jon Nehring <jwn@stonetip.com>
2017-03-25 19:15:08 -0600

    Working version with heading only

--- locTestA/locTestA/ViewController.swift
+++ locTestA/locTestA/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
     
     @IBOutlet weak var labelLatVal: UILabel!
     @IBOutlet weak var labelLonVal: UILabel!
@@ -21,18 +22,78 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnStop: UIButton!
     
     
+    let locMgr = CLLocationManager()
+    var updating = false
     
-
+  
+    @IBAction func doStart (_ sender: Any!){
+        
+        guard CLLocationManager.headingAvailable() else {
+            
+            print("No hardware found")
+            return
+        } // no hardware (Note: remove or modify once lat/lon is used too
+        
+        if self.updating {return}
+        if self.locMgr.delegate == nil { self.locMgr.delegate = self }
+        
+        print("starting")
+        
+        self.locMgr.headingFilter = 1
+        self.locMgr.headingOrientation = .portrait
+        self.updating = true
+        
+        self.locMgr.startUpdatingHeading()
+    }
+    
+    
+    @IBAction func doStop (_ sender: Any!){
+        
+        print("stopping")
+        
+        self.locMgr.stopUpdatingHeading()
+        self.updating = false
+        
+        labelHeadingVal.text = String(format: "%.1f", 0)
+        //TODO: reset label values
+    }
+   
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        self.doStop(nil)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        
+        var currentHeading = newHeading.magneticHeading
+        
+        let trueHeading = newHeading.trueHeading // - 1 if no location info
+        
+        print("ch: \(currentHeading) th: \(trueHeading)")
+        
+        if trueHeading > 0 {
+            
+            currentHeading = trueHeading
+        }
+        
+        labelHeadingVal.text = String(format: "%.1f", currentHeading)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
